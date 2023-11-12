5ada3f69991f7e81f2887b7cdf8e69525d1ddc69
marc0246 <40955683+marc0246@users.noreply.github.com>
2023-11-12 11:00:29 +0100

    Remove copyright notices from source files
    See vulkano-rs/vulkano#2401 for rationale.

--- chapter-code/05-images/mandelbrot.rs
+++ chapter-code/05-images/mandelbrot.rs
@@ -1,12 +1,3 @@
-// Copyright (c) 2017 The vulkano developers
-// Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT
-// license <LICENSE-MIT or http://opensource.org/licenses/MIT>,
-// at your option. All files in the project carrying such
-// notice may not be copied, modified, or distributed except
-// according to those terms.
-
 //! This is the source code of the "Drawing a fractal with a compute shader" subchapter
 //! from the "Using images" chapter at http://vulkano.rs.
 //!
